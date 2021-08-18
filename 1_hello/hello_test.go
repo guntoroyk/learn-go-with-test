ec7938d50a4757594df7f40b56516f67d4d90c71
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-18 20:37:22 +0700

    refactor: add subtests and helper function

--- 1_hello/hello_test.go
+++ 1_hello/hello_test.go
@@ -3,11 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Guntoro")
-
-	want := "Hello, Guntoro"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB /* an interface that *testing.T & *testing.B both satisfy*/, got, want string) {
+		t.Helper() // tell the test suit that this method is a helper
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Guntoro")
+
+		want := "Hello, Guntoro"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
