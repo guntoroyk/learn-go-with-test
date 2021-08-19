bd56eee36d1ba5353314ca6d3a887a09bcf13ce1
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-19 10:03:32 +0700

    feat: Area functions for circles

--- 5_structs_methods_interfaces/main.go
+++ 5_structs_methods_interfaces/main.go
@@ -1,8 +1,22 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
