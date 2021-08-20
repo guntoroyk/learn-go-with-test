cf565a31dffda5c194d2e36cf4875cad59e24e9e
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-20 09:24:43 +0700

    refactor: make sure test output is helpful;

--- 5_structs_methods_interfaces/main_test.go
+++ 5_structs_methods_interfaces/main_test.go
@@ -13,15 +13,15 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			if got != want {
-				t.Errorf("got %g want %g", got, want)
-			}
-		}
-	}
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		if got != want {
+	// 			t.Errorf("got %g want %g", got, want)
+	// 		}
+	// 	}
+	// }
 
 	// t.Run("rectangles", func(t *testing.T) {
 	// 	rectangle := Rectangle{10.0, 10.0}
@@ -34,15 +34,21 @@ func TestArea(t *testing.T) {
 	// })
 
 	allShapes := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{10.0, 10.0}, 100.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{10.0, 10.0}, hasArea: 100.0},
+		{name: "Circle", shape: Circle{10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range allShapes {
-		checkArea(t, tt.shape, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
