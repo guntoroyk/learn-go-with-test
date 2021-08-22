16a3731a499990b7ff115872c036746d568a6dcb
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-23 06:45:35 +0700

    feat: add error error handling if word is not found

--- 7_maps/dictionary.go
+++ 7_maps/dictionary.go
@@ -1,7 +1,17 @@
 package maps
 
+import (
+	"errors"
+)
+
 type Dictionary map[string]string
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
