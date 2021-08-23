7625d85171a44704582d7d402cb36a13bbbb3ccc
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-23 07:41:18 +0700

    refactor: made the errors constant with DictionaryErr type

--- 7_maps/dictionary.go
+++ 7_maps/dictionary.go
@@ -1,13 +1,17 @@
 package maps
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExist = errors.New("could not add the word because it already exists")
+const (
+	ErrNotFound  = DictionaryErr("could not find the word you were looking for")
+	ErrWordExist = DictionaryErr("could not add the word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
