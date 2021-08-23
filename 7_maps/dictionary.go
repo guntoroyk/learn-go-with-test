cac365a2fe2fbd3dfacaa4c6cbf614bbf285d90c
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-23 07:21:12 +0700

    feat: add validation to check existing word when Add new word

--- 7_maps/dictionary.go
+++ 7_maps/dictionary.go
@@ -7,6 +7,7 @@ import (
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExist = errors.New("could not add the word because it already exists")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -18,6 +19,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word string, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExist
+	default:
+		return err
+	}
+
+	return nil
 }
